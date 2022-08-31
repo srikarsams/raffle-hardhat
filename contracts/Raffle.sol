b8e2ab539aac0e1f10c216a077c3033c1f2d9267
srikar-samudrala <srikar@eve.world>
2022-08-31 14:41:43 +0530

    initial setup

---
+++ contracts/Raffle.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+// Enter the lottery by paying pre-decided amount (Eg: 0.1ETH)
+// Pick a random winner (using ChainLink keeper)
+// Winner will be automatically selected every x min
+
+pragma solidity 0.8.9;
+
+contract Raffle {}
